5f2a6bd14483653bf4dee5d1d26f1c4b0d54ed39
PJuanTuiran <juanfer20091403@gmail.com>
2025-02-04 17:01:53 +0000

    feat: add a new configuration in vite file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",  // Asegura que escuche en todas las IPs
+    port: 8050,       // Usa el puerto correcto
+    strictPort: true,
+    allowedHosts: ["auth.parfinanciero.crudzaso.com"]
+  }
 })
