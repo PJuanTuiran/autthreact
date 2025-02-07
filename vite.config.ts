0d23812854480c0e12f8576d3145ecdef0c7c4ac
PJuanTuiran <juanfer20091403@gmail.com>
2025-02-07 00:29:43 -0500

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,50 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { federation } from "@module-federation/vite";
-import { dependencies } from "./package.json";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { federation } from '@module-federation/vite'
+import { dependencies } from './package.json';
 
-export default defineConfig(({ command }) => ({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [
+
+    
     federation({
-      name: "remote",
-      filename: "remoteEntry.js",
+      name: 'remote', // Nombre del remote
+      filename: 'remoteEntry.js',
       exposes: {
-        "./Login": "./src/Login.tsx",
-        "./AuthContext": "./src/context/AuthContext.tsx",
+        './Login': './src/Login.tsx', // Expone el componente Login
+        './AuthContext': './src/context/AuthContext.tsx', // Expone el AuthContext
       },
       shared: {
         react: { singleton: true, requiredVersion: dependencies.react },
-        "react-dom": { singleton: true, requiredVersion: dependencies["react-dom"] },
+        'react-dom': { singleton: true, requiredVersion: dependencies['react-dom'] },
       },
+
+      
     }),
-    react(),
-  ],
 
-  build: {
-    target: "chrome89",
+    react()],
+  
+    build: {
+    target: 'chrome89',
   },
-
   server: {
-    host: "0.0.0.0",
-    port: 8050,
+    host: "0.0.0.0",  // Asegura que escuche en todas las IPs
+    port: 8050,       // Usa el puerto correcto
     strictPort: true,
 
-    cors: {
-      origin: "*",
-      methods: ["GET", "HEAD", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    },
 
+    cors: {
+      origin: "*", // Permite acceso desde cualquier origen
+      methods: ["GET", "HEAD", "OPTIONS"], // Métodos permitidos
+      allowedHeaders: ["Content-Type", "Authorization"], // Headers permitidos
+    },
     allowedHosts: ["auth.parfinanciero.crudzaso.com"],
 
-    hmr: command === "serve" ? { 
-      protocol: "wss", 
-      host: "auth.parfinanciero.crudzaso.com",
-      port: 443 
-    } : false, // ❌ Deshabilita HMR en producción
-  },
-}));
+     hmr: {
+      protocol: "wss", // Usa WebSockets seguros en producción
+      host: "auth.parfinanciero.crudzaso.com", // Dirección del servidor en producción
+      port: 443, // WebSockets en HTTPS corren en el puerto 443
+    },
+  }
+})
