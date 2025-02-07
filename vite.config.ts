aa692be92306bfc4df4e802ca0d039a0867229f2
PJuanTuiran <juanfer20091403@gmail.com>
2025-02-06 22:06:25 -0500

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { federation } from '@module-federation/vite'
+import { dependencies } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+
+    
+    federation({
+      name: 'remote', // Nombre del remote
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Login': './src/Login.tsx', // Expone el componente Login
+        './AuthContext': './src/context/AuthContext.tsx', // Expone el AuthContext
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: dependencies.react },
+        'react-dom': { singleton: true, requiredVersion: dependencies['react-dom'] },
+      },
+
+      
+    }),
+
+    react()],
+  
+    build: {
+    target: 'chrome89',
+  },
   server: {
     host: "0.0.0.0",  // Asegura que escuche en todas las IPs
     port: 8050,       // Usa el puerto correcto
